20f127dfc114393d4d0acc941a321c43dc8f64f9
jeroldan <jeroldan@gmail.com>
2015-08-05 13:43:35 +0200

    flexbox layout

--- js/apps/index/main.js
+++ js/apps/index/main.js
@@ -1,47 +1,4 @@
 define([require, '../../vendor/chips/index'], function (require, chips) {
-    
-    var app = chips(page);
-   
-    app.chips.register('chips-input', {
-        is : 'input',
-        model: { value: 'default'}
-    });
-   
-    app.chips.register('index-field', {
-        is: 'div',
-        model: {label: '', value: 'Hello World', icon: ''},
-        view: {
-            /*
-            icon1: {
-                is: 'chips-icon',
-                model: {src: '{host.icon}'},
-                layout: {row: 1, colspan: 1, position: 1},
-                style: {}
-            },
-            label1: {
-                is: 'chips-label',
-                model: {text: '{host.model.label}'},
-                layout: {row: 1, colspan: 4, position: 1},
-                style: {}
-            },
-            */
-            textbox1: {
-                is: 'chips-input',
-                model: {value: '{{host.model.value}}'},
-                layout: {row: 1, colspan: 7, position: 1},
-                style: {}
-            },
-            /*
-            message1: {
-                is: 'chips-label',
-                model: {value: '{{host.view.textbox1.model.error}}'},
-                layout: {row: 2, colspan: 12, position: 1},
-                style: {}
-            }*/
-        },
-        layout: {rows: 2, columns: 12},
-        style: {}
-    });
 
     var page = {
         init: function () {
@@ -52,7 +9,34 @@ define([require, '../../vendor/chips/index'], function (require, chips) {
         }
     };
 
-//    window.addEventListener("load", app.kernel.boot, false);
-//    window.addEventListener("unload", app.kernel.destroy, false);
+    var app = chips(page);
 
+    /**
+     * Usage:
+     * <index-field icon="icon1" label="xxxx" value="yyyy"></index-field>
+     */
+    app.chips.register('index-field', {
+        is: 'div',
+        attributes: ['icon', 'label', 'value'],
+        model: {value: '{value}'},
+        view: {
+            icon1: {
+                is: 'img',
+                attributes: {src: '{icon}'}
+            },
+            label1: {
+                is: 'label',
+                attributes: {text: '{label}'}
+            },
+            textbox1: {
+                is: 'input',
+                attributes: {value: '{value}'}
+            },
+            message1: {
+                is: 'span',
+                attributes: {value: '{model.value}'}
+            }
+        }
+    });
 });
+
--- js/vendor/chips/lib/chips.js
+++ js/vendor/chips/lib/chips.js
@@ -6,43 +6,80 @@ define(function () {
 
                 var Element = React.createClass({
                     displayName: tagName,
+                    getInitialState: function () {
+
+                        var state = Object.assign({}, chip.model);
+                        
+                        // project attributes to model << REFACTOR
+                        var _props = this.props;
+                        Object.keys(state).forEach(function (key) {
+                            var matches = state[key].match(/\{([^\}]+)\}(?![\S\s]*\1)/gi);
+                            if (matches)
+                                matches.forEach(function (match) {
+                                    var m = match.slice(1, -1);
+                                    state[key] = _props[m];
+                                });
+                        });
+                        
+                        return state;
+                    },
+                    componentWillMount: function () {
+                        
+                    },
                     render: function () {
-
-                        var props = {};
-                        if (chip.model) {
-                            props = Object.assign({}, chip.model, this.props);
-                        }
-
+                        
+                        // project parent properties and state to children props
+                        var props = Object.assign({}, this.props, this.state, {onChange: this.changeHandler});
                         var items = [];
-                        if (chip.view) {
-                            Object.keys(chip.view).forEach(function (key) {
-                                var item = chip.view[key];
-                                var itemProps = {};
-                                if (item.model) {
-                                    itemProps = Object.assign({}, item.model, props);
-                                }
-                                items.push(React.createElement(item.is, itemProps));
+                        var view = Object.assign({}, chip.view);
+                        Object.keys(view).forEach(function (key) {
+                            var item = view[key];
+                            var attributes = Object.assign({}, item.attributes);
+                            Object.keys(attributes).forEach(function (key) {
+                                var matches = attributes[key].match(/\{([^\}]+)\}(?![\S\s]*\1)/gi);
+                                if (matches)
+                                    matches.forEach(function (match) {
+                                        var m = match.slice(1, -1);
+                                        attributes[key] = props[m];
+                                    });
                             });
-                        }
-
+                            console.log(item.is+' :');
+                            items.push(React.createElement(item.is, attributes, [attributes['text']]));
+                        });
+                        
+                        items.push(this.state.text);
                         return (React.createElement(chip.is, props, items));
+                    },
+                    componentDidMount: function () {
+                        console.log('React component did mount');
+                    },
+                    changeHandler: function (e) {
+                        if (e.type === 'input')
+                            this.setState({value: e.target.value});
                     }
                 });
 
                 var proto = Object.create(HTMLElement.prototype);
 
                 proto.createdCallback = function () {
+                    console.log('WC created callback');
+                    // project HTMLElement attributes to chip attributes << REFACTOR
                     var attrs = {};
                     if (this.attributes) {
                         for (var i = 0; i < this.attributes.length; i++) {
-                            attrs[this.attributes[i].name] = this.attributes[i].value;
+                            var attributeName = this.attributes[i].name;
+                            if (chip.attributes.indexOf(attributeName) >= 0)
+                                attrs[attributeName] = this.attributes[i].value;
+                            else
+                                console.warn('Warning: the attribute ' + attributeName + ' is not published by tag ' + tagName);
                         }
                     }
-                    // create React Element passing tag attributes as properties
+                    // create React Element passing tag attributes to properties
                     this.element = React.createElement(Element, attrs);
                 };
 
                 proto.attachedCallback = function () {
+                    console.log('WC attached callback');
                     // render React element
                     React.render(this.element, this);
                 };
@@ -52,7 +89,7 @@ define(function () {
                 };
 
                 proto.attributeChangedCallback = function (attrName, oldVal, newVal) {
-                    // TODO:
+                    console.log(attrName);
                 };
 
                 // register the new webcomponent
